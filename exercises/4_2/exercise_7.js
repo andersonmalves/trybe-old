1e31836f866d126d66ef70702f06b4ccafd341c5
Anderson Meira <contatodeanderson@gmail.com>
2020-07-14 19:43:58 -0300

    Comecei o exercício 7

--- exercises/4_2/exercise_7.js
+++ exercises/4_2/exercise_7.js
@@ -1,5 +1,19 @@
 /*
-
+  Utilizando for, descubra qual o menor valor contido no array e imprima-o; 
 */
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let menorValor = 0;
+
+for (let pos = 0; pos < numbers.length; pos += 1) {
+  
+  valorAtual = numbers[pos];
+
+  if (numbers[pos] < valorAtual) {
+    menorValor = numbers[pos];
+  }
+}
+
+console.log('Valor menor é: ' + menorValor);
+
