cf832a5fdd619efa6785633baab3626bbc333550
Anderson Meira <contatodeanderson@gmail.com>
2020-07-14 18:43:23 -0300

    Sexto exercício

--- exercises/4_2/exercise_6.js
+++ exercises/4_2/exercise_6.js
@@ -1,5 +1,20 @@
 /*
-
+  Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
 */
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let totalImpar = 0;
+
+for (let pos = 0; pos < numbers.length; pos += 1) {
+  if (numbers[pos] % 2 !== 0) {
+    totalImpar += 1;
+  }
+}
+
+if (totalImpar != 0) {
+  console.log('Números ímpares: ' + totalImpar);
+} else {
+  console.log('nenhum valor ímpar encontrado');
+}
+
